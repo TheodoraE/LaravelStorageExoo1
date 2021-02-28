07b476e2e51e9e37861a6975a1e6c715aa0ab380
TheodoraE <tidoraa@gmail.com>
2021-02-28 20:34:34 +0100

    controllers

--- app/Http/Controllers/BackController.php
+++ app/Http/Controllers/BackController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class BackController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('backoffice.admin');
+    }
+
+    public function create()
+    {
+        return view('backoffice.pages.fichier');
+    }
+
+    public function store(Request $request)
+    {
+        Storage::put('public/img', $request->file('name'));
+
+        $store = new File;
+        $store->name = $request->name;
+        $store->save();
+
+        return view('backoffice.admin');
+    }
 }
--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('accueil');
+    }
+
+    
 }
